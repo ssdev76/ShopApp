2f2b60e6be3a2626b5ccd5676a9281288d3c1f78
Test <Test>
2019-02-11 00:00:08 +0300

    no message

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import './loader.css';
 
 const Loader = (props) => {
   const { errorMessage } = props;
